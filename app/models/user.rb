9101f5f40bdd9a042a14bdc96970cdac36135bcc
Sebastien <sebastien@3sparks.net>
2013-08-21 23:39:42 -0400

    added test for user first name last name and profile name spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,17 @@ class User < ActiveRecord::Base
   				:first_name, :last_name, :profile_name
   # attr_accessible :title, :body
   
+  validates :first_name, presence: true
+  
+  validates :last_name, presence: true
+  
+  validates :profile_name, presence: true,
+  						   uniqueness: true,
+  						   format:{
+  						   		with: /\A[a-zA-Z\-\_]+\Z/,
+  						   		message: "Must be formatted correctly."
+  						   }
+  
   has_many :statuses
   
   def full_name
